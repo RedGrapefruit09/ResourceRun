0600809a7df8e18578b60ab270db0a47aec98b5a
RedGrapefruit09 <karpovanton729@gmail.com>
2022-05-02 13:17:50 +0300

    Add automatic creation of world borders

--- Assets/Scripts/WorldGenerator.cs
+++ Assets/Scripts/WorldGenerator.cs
@@ -16,6 +16,7 @@ public class WorldGenerator : MonoBehaviour
     private void Generate()
     {
         GenerateGrass();
+        GenerateWorldBorders();
     }
 
     private void GenerateGrass()
@@ -29,6 +30,41 @@ public class WorldGenerator : MonoBehaviour
                 groundTilemap.SetTile(new Vector3Int(x, y, 0), GetGrassTile(x, y));
             }
         }
+    }
+
+    private void GenerateWorldBorders()
+    {
+        var borderParentObject = new GameObject { name = "World Borders" };
+        
+        void CreateBorder(Vector2 pos, Vector2 size, string borderName)
+        {
+            var obj = new GameObject();
+            obj.transform.parent = borderParentObject.transform;
+            obj.transform.position = pos;
+            obj.name = borderName;
+            var boxCollider = obj.AddComponent<BoxCollider2D>();
+            boxCollider.size = size;
+        }
+        
+        CreateBorder(
+            new Vector2(0f, -(worldHeight / 2f) - 0.5f),
+            new Vector2(worldWidth, 1f),
+            "Bottom World Border");
+        
+        CreateBorder(
+            new Vector2(0f, worldHeight / 2f + 0.5f),
+            new Vector2(worldWidth, 1f),
+            "Top World Border");
+        
+        CreateBorder(
+            new Vector2(-(worldWidth / 2f) - 0.5f, 0f),
+            new Vector2(1f, worldHeight),
+            "Left World Border");
+        
+        CreateBorder(
+            new Vector2(worldWidth / 2f + 0.5f, 0f),
+            new Vector2(1f, worldHeight),
+            "Right World Border");
     }
 
     private Tile GetGrassTile(int x, int y)
